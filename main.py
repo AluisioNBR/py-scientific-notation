4a70efd64a253b92ed71188a18f52ea650c5fa63
AluísioNBR <lulu70638@gmail.com>
2021-11-17 11:29:24 -0300

    Ajusted

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-msg = """=========================================
+from time import sleep 
+
+msgs = [
+"""=========================================
      Conversor de Notação Científica
 =========================================
+""",
 
+"""
 Qual operação deseja realizar ?
 
 1 - Notação científica >>>> Numérico
 2 - Numérico >>>> Notação científica
 3 - Sair
 
->>>>>>>>>> """
+>>>>>>>>>> """,
 
-#while True:
-op = input(msg)
+"""
+      OPÇÃO INVÁLIDA!!
+
+Tente novamente..."""
+]
+
+print(msgs[0])
+
+while True:
+     op = int(input(msgs[1]))
+
+     if op == 3:
+          break
+
+     elif op == 1:
+          # Código...
+          print()
+     
+     elif op == 2:
+          # Código...
+          print()
+
+     else :
+          print(msgs[2])
+
+print("\nEncerrando...\n", end=""); sleep(3)
