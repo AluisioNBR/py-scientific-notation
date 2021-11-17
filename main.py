2537099132af3ce7dbe33984349c3e8260c53c85
AluísioNBR <lulu70638@gmail.com>
2021-11-16 22:24:56 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+msg = """=========================================
+     Conversor de Notação Científica
+=========================================
+
+Qual operação deseja realizar ?
+
+1 - Notação científica >>>> Numérico
+2 - Numérico >>>> Notação científica
+3 - Sair
+
+>>>>>>>>>> """
+
+#while True:
+op = input(msg)
